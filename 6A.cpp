9097925512dabe2de40bd2c23a815f97b2915bcf
Curry-codes <carismageorge@gmail.com>
2024-10-08 15:58:26 +0530

    Update 6A.cpp

--- 6A.cpp
+++ 6A.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 #define max 10
 
-int a[11] = {0, 10, 14, 19, 26, 27, 31, 33, 35, 42, 44}; // 0 is the first element
+int a[11] = {10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0}; 
 int b[10];
 
 void merging(int low, int mid, int high) {
